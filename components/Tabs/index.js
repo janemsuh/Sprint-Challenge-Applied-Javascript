1aca807535b3974abd384e4592e2393284d769b0
Jane Suh <sourcherryyogurt@gmail.com>
2019-10-11 19:41:13 -0400

    Added Tab component

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,27 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const topics = document.querySelector('.topics');
+
+axios
+    .get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(response => {
+        console.log(response);
+        response.data.topics.forEach(item => {
+            topics.appendChild(tabCreator(item));
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+
+function tabCreator(data) {
+
+    const tab = document.createElement('div');
+    tab.classList.add('tab');
+    tab.textContent = data;
+
+    return tab;
+}
